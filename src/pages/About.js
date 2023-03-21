86249bf2e9f4e99a09973fdd2c64c6804605b0ce
Emmanuelle Jupon <emmanuellejupon@gmail.com>
2023-03-21 18:52:02 +0100

    insertion autoportrait et creation page about

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,26 +2,26 @@ import React from 'react';
 
 const About = () => {
     return (
-        <section className='about dark'>
-            <div className="background">
-                <div className="text">
-                    <h1>A propos</h1>
-                    <p>
-                        Hello, Je suis Emmanuelle Jupon et j'utilise rayhearth pour les réseaux sociaux. Je suis graphiste et développeuse web front-end.
-                    </p>
-                    <p>
-                        J'ai suivi une prépa aux arts appliqués puis un BTS communication visuelle durant 3 ans avant de me lancer dans la vie active. Durant plusieurs années, j’ai occupé des postes de rédacteurs administratifs dans des secteurs totalement variés de l’édition à l’industrie en passant par l’architecture et l’ingenierie. Dans tous les postes que j’ai occupé, j’ai toujours fait appel à mes connaissances et à ma culture graphique, je suis une passionnée d’informatique. C’est pourquoi j’ai décidé de reprendre mes études et me former au métier de webdesigner et développeur afin d’acquérir les compétences nécessaires dans la création de site web UI/UX.
-                    </p>
-                    <p>
-                        Aujourd’hui, je suis plus que jamais passionnée par mon métier, je conçois et réalise des sites web du cahier des charges à la mise en ligne, en passant par la conception graphique, la prise de vue photographique et le responsive.Si mon travail vous plaît, n’hésitez pas à embarquer avec moi pour révéler votre projet...
-                    </p>
+        <section className='about'>
 
-                </div>
-                <div className="portrait">
-                    <img src="" alt="" />
-                </div>
+            <div className="text">
+                <h1>A propos</h1>
+                <p>
+                    Hello, Je suis Emmanuelle Jupon, je suis graphiste et développeuse web front-end.
+                </p>
+                <p>
+                    J'ai suivi une prépa aux arts appliqués puis un BTS communication visuelle durant 3 ans avant de me lancer dans la vie active. Durant plusieurs années, j’ai occupé des postes de rédacteurs administratifs dans des secteurs totalement variés de l’édition à l’industrie en passant par l’architecture et l’ingenierie. Dans tous les postes que j’ai occupé, j’ai toujours fait appel à mes connaissances et à ma culture graphique, je suis une passionnée d’informatique. C’est pourquoi j’ai décidé de reprendre mes études et me former au métier de webdesigner et développeur afin d’acquérir les compétences nécessaires dans la création de site web UI/UX.
+                </p>
+                <p>
+                    Aujourd’hui, je suis plus que jamais passionnée par mon métier, je conçois et réalise des sites web du cahier des charges à la mise en ligne, en passant par la conception graphique, la prise de vue photographique et le responsive.Si mon travail vous plaît, n’hésitez pas à embarquer avec moi pour révéler votre projet...
+                </p>
 
             </div>
+            <div className="portrait">
+                <img src="./assets/img/autoportrait-23.png" alt="" />
+            </div>
+
+
         </section>
     );
 };
